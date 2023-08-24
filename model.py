7b87e700aace7071aef8588d62d3d820f6a809ca
Karamvir <karam.hira10@gmail.com>
2023-08-24 10:38:20 -0400

    added training loop

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import torch.nn.functional as F
+
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,6 +11,7 @@ matplotlib.use('TkAgg')
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 
@@ -21,22 +22,26 @@ dataSet = torchvision.datasets.EMNIST(
 )
 
 letterCategories = dataSet.classes[1:]
-dataSet.classes = letterCategories
 labels = copy.deepcopy(dataSet.targets) - 1
-dataSet.targets = labels
+torch.unique(labels)
+#target_to_label = {target: letter for target, letter in enumerate(letterCategories)}
+images = dataSet.data.view([124800, 1, 28, 28]).float()
+
 
 print(str(len(dataSet.classes)) + ' classes')
 print('\n datasize')
 print(dataSet.data.shape) 
 print(dataSet.classes)
 
-for x in range(5):
-    img = dataSet.data[x].numpy()
-    print(repr(img))
-    img = img.T
-    plt.imshow(img, cmap='gray')
-    plt.axis('off')
-    plt.show()
+print(target_to_label)
+
+#for x in range(5):
+#    img = dataSet.data[x].numpy()
+#    print(repr(img))
+#    img = img.T
+#    plt.imshow(img, cmap='gray')
+#    plt.axis('off')
+#    plt.show()
 
 #plt.hist(images[:10].view(-1).detach(), bins=40)
 #plt.title("bottle")
@@ -60,11 +65,11 @@ train_data, test_data, train_labels, test_labels = train_test_split(
 train_data = TensorDataset(train_data, train_labels)
 test_data = TensorDataset(test_data, test_labels)
 
-
 train_loader = torch.utils.data.DataLoader(
     train_data, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(
     test_data, batch_size=batch_size, shuffle=True)
+
 
 device = (
     "cuda"
@@ -79,9 +84,7 @@ print(f"Using {device} device")
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # 1 input channel, 32 output channels, 3x3 kernel, stride 1
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        # 32 input channels, 64 output channels, 3x3 kernel, stride 1
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         
         self.conv3 = nn.Conv2d(64, 128, 3, 1)
@@ -89,8 +92,8 @@ class Net(nn.Module):
         self.pool1 = nn.MaxPool2d(2, 2)
         self.pool2 = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(1152, 128)
+        self.fc2 = nn.Linear(128, 26)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -115,15 +118,35 @@ class Net(nn.Module):
         return output
 
 model = Net()
+model.to(device)
+
 loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=.001)
- 
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
 num_epochs = 20
 for epoch in range(num_epochs):
+    model.train()
     for batch_idx, (features, targets) in enumerate(train_loader):
-
+        features, targets = features.to(device), targets.to(device)
+        optimizer.zero_grad()
         forward_pass_outputs = model(features)
         loss = loss_fn(forward_pass_outputs, targets)
-        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+    # Print training loss and accuracy
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for features, targets in test_loader:
+            features, targets = features.to(device), targets.to(device)
+            outputs = model(features)
+            _, predicted = torch.max(outputs.data, 1)
+            total += targets.size(0)
+            correct += (predicted == targets).sum().item()
+    accuracy = correct / total
+    print(f"Epoch [{epoch+1}/{num_epochs}] Loss: {loss:.4f} Accuracy: {accuracy:.4f}")
+
+torch.save(model.state_dict(), "emnist_letter_model.pth")
+
